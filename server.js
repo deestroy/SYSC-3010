19b637c4e7e6310efeda2ce58d2f728640d50f59
ThomasJKnechtel <thomasknechtel@cmail.carleton.ca>
2022-03-10 23:31:05 -0500

    connect to firebase

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser'
 const server = express();
+
+import { initializeApp } from 'firebase/app'
+import { getDatabase} from 'firebase/database'
+const firebaseConfig = {
+    apiKey: "AIzaSyAmHtdWuIyvGzFkxE_NNc7hMBMIDZ4eG7s",
+    authDomain: "sysc3010-project-l1g3.firebaseapp.com",
+    databaseURL: "https://sysc3010-project-l1g3-default-rtdb.firebaseio.com",
+    storageBucket: "sysc3010-project-l1g3.appspot.com"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app)
+
+//server.use(express.static(__dirname+'/public')) //static files location
+server.use(bodyParser.urlencoded({'extended':'true'}))
+server.use(bodyParser.json())
 
 server.get('/',(req,res)=>{
     res.send('A simple Node app is runing on this server')
