aaa32c373560eade4dd09d958ac6771918870aea
ThomasJKnechtel <thomasknechtel@cmail.carleton.ca>
2022-03-11 01:47:26 -0500

    added pages

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import { sendToRPI_Controller } from './send_datagram.js';
-const server = express();
-
 import { initializeApp } from 'firebase/app'
 import { getDatabase, ref, onValue} from 'firebase/database'
+import { verify } from './login_controller';
+
+const server = express();
 const firebaseConfig = {
     apiKey: "AIzaSyAmHtdWuIyvGzFkxE_NNc7hMBMIDZ4eG7s",
     authDomain: "sysc3010-project-l1g3.firebaseapp.com",
@@ -19,7 +20,7 @@ onValue(userRef,(snapshot)=>{   //Listen for update
     const data = snapshot.val();
     console.log(data)
 })
-//server.use(express.static(__dirname+'/public')) //static files location
+server.use(express.static(__dirname+'/public')) //static files location
 server.use(bodyParser.urlencoded({'extended':'true'}))
 server.use(bodyParser.json())
 
@@ -27,6 +28,9 @@ server.get('/',(req,res)=>{
     res.send('A simple Node app is runing on this server')
     res.end();
 })
+server.get('/login',verify(req, res, (req, res)=>{
+    res.sendFile('./public/home.html')
+}))
 server.post('/display', (req, res)=>{
     sendToRPI_Controller("This is test",8080,'localhost')
     res.statusCode(200)
