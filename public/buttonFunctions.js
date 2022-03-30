f998b3bccef1cc702ca260a433556d9c3c57ae59
Thomas <thomasknechtel@outlook.com>
2022-03-29 21:54:15 -0400

    eslint complete for buttonFunctions.js

--- public/buttonFunctions.js
+++ public/buttonFunctions.js
@@ -1,52 +1,41 @@
-function scan(){
-    var req = new XMLHttpRequest()
-    req.open('GET','/scan')
-    req.send()
+function scan() {
+  const req = new XMLHttpRequest();
+  req.open('GET', '/scan');
+  req.send();
 }
 
-function rescan(){
-    var items = document.getElementsByClassName('clicked')
-    var itemList = []
-    for(var item of items){
-        itemList.push(item.children.item(0).innerHTML)
-    }
-    var req = new XMLHttpRequest()
-    req.open('post', '/rescan')
-    req.setRequestHeader('Content-Type','application/json')
-    req.send(JSON.stringify(itemList))
+function rescan() {
+  const items = document.getElementsByClassName('clicked');
+  const itemList = [];
+  items.map((item) => itemList.push(item.children.item(0).innerHTML));
+  const req = new XMLHttpRequest();
+  req.open('post', '/rescan');
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(JSON.stringify(itemList));
+}
+const req = new XMLHttpRequest();
+function getItems() {
+  req.open('GET', '/user/User0');
 
+  req.send();
 }
-var req = new XMLHttpRequest()
-function getItems(){
-   
-    req.open('GET','/user/User0') 
-    
-    req.send()
-   
-    
-    
+req.onload = function () {
+  const items = req.response.Calorie_Count;
+  Object.keys(items).forEach((key) => {
+    addItem(JSON.parse(items[key]));
+  });
+};
+function calculate() {
+  const items = document.getElementsByClassName('clicked');
+  const itemList = [];
+  items.map((item) => itemList.push(parseInt(item.children.item(1).innerHTML)));
+  const calories = calculateCalories(itemList);
+  document.getElementById('displayCals').innerHTML = calories;
 }
-req.onload=function(){
-        var items= req.response.Calorie_Count
-        for(item in items){
-            addItem(JSON.parse(item))
-    }
-}
-function calculate(){
-    var items = document.getElementsByClassName('clicked')
-    var itemList = []
-    for(var item of items){
-        itemList.push(parseInt(item.children.item(1).innerHTML))
-    }
-    var calories = calculateCalories(itemList)
-    document.getElementById('displayCals').innerHTML=calories
-}
-function clickHandler(elem){
-    if(elem.classList.contains('clicked')){
-        elem.classList.remove('clicked')
-    }else{
-         elem.classList.add('clicked')
-    }
-   
-
+function clickHandler(elem) {
+  if (elem.classList.contains('clicked')) {
+    elem.classList.remove('clicked');
+  } else {
+    elem.classList.add('clicked');
+  }
 }
