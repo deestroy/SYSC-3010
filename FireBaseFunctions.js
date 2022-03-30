d68db95ed268231940981c7e2d473c0efa2e18da
Thomas <thomasknechtel@outlook.com>
2022-03-29 23:31:43 -0400

    ESLint for files

--- FireBaseFunctions.js
+++ FireBaseFunctions.js
@@ -1,46 +1,57 @@
-import { getDatabase, ref, onValue, get, child, push, set} from 'firebase/database'
-import { initializeApp } from 'firebase/app'
-var data
+import {
+  getDatabase, ref, get, child, push, set,
+} from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAmHtdWuIyvGzFkxE_NNc7hMBMIDZ4eG7s",
-    authDomain: "sysc3010-project-l1g3.firebaseapp.com",
-    databaseURL: "https://sysc3010-project-l1g3-default-rtdb.firebaseio.com",
-    storageBucket: "sysc3010-project-l1g3.appspot.com"
+  apiKey: 'AIzaSyAmHtdWuIyvGzFkxE_NNc7hMBMIDZ4eG7s',
+  authDomain: 'sysc3010-project-l1g3.firebaseapp.com',
+  databaseURL: 'https://sysc3010-project-l1g3-default-rtdb.firebaseio.com',
+  storageBucket: 'sysc3010-project-l1g3.appspot.com',
 };
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app)
-const userRef = ref(db)
-
-async function getUserData(userid){
-    
-   await get(child(userRef,userid)).then((snapshot)=>{
-        if(snapshot.exists()){
-            var val=snapshot.val()
-            data=val
-            console.log(data)
-            return val
-        }else{
-            return null
-        }
-    }).catch((error) => {
-        console.error(error);
-
-      });
-}
-function pushUser(userid, user_email){
-  
-    if(getUserData(userid)==null){
-        console.log('ran')
-        var baseRef = ref(db,userid)
-        var writeRef = push(baseRef)
-        set(writeRef, {
-            email: user_email,
-            RPI_Controller_Adress:'192.168.2.174',
-            RPI_Display_Adress:'192.170.2.174'
-        })
-        
+const db = getDatabase(app);
+const userRef = ref(db);
+/**
+ * returns the User object for a userid.
+ * @param userid A user's google userid string
+ */
+async function getUserData(userid) {
+  await get(child(userRef, userid)).then((snapshot) => {
+    if (snapshot.exists()) {
+      const val = snapshot.val();
+      return val;
     }
-    return false;
-   
+    return null;
+  }).catch((error) => {
+    console.error(error);
+  });
 }
-export { getUserData, pushUser, data}
+/**
+ * Pushes a new User object to the firebase database
+ * @param {*} userid the google id string of the user
+ * @param {*} user_email the email of the user
+ * @returns true if the user was added false if it wasnt
+ */
+function pushUser(userid, userEmail) {
+  if (getUserData(userid) == null) {
+    const baseRef = ref(db, userid);
+    const writeRef = push(baseRef);
+    set(writeRef, {
+      email: userEmail,
+      RPI_Controller_Adress: '192.168.2.174',
+      RPI_Display_Adress: '192.170.2.174',
+    });
+    return true;
+  }
+  return false;
+}
+export { getUserData, pushUser };
+/*  Style used: airbnb. FLAKE8 can not be used for JavaScript. ESLint output:
+C:\Users\Thomas\Documents\3010Project\SYSC-3010\FireBaseFunctions.js
+  27:5  warning  Unexpected console statement  no-console
+
+✖ 1 problem (0 errors, 1 warning)
+
+The no-console warning was ignored as the console output prints out an error message if error occurs.
+ */
--- public/buttonFunctions.js
+++ public/buttonFunctions.js
@@ -1,9 +1,14 @@
+/**
+ * Sends a scan get request to server.js.
+ */
 function scan() {
   const req = new XMLHttpRequest();
-  req.open('GET', '/scan');
+  req.open('get', '/scan');
   req.send();
 }
-
+/**
+ * sends a rescan POST request to the server. Passes all items that are clicked in the POST body.
+ */
 function rescan() {
   const items = document.getElementsByClassName('clicked');
   const itemList = [];
@@ -14,24 +19,37 @@ function rescan() {
   req.send(JSON.stringify(itemList));
 }
 const req = new XMLHttpRequest();
+/**
+ * Sends a GET request to get all items for the user with userID = User0
+ */
 function getItems() {
   req.open('GET', '/user/User0');
 
   req.send();
 }
-req.onload = function () {
+/**
+ * When GET 'user/User0' loads add items to table
+ */
+req.onload = () => {
   const items = req.response.Calorie_Count;
   Object.keys(items).forEach((key) => {
     addItem(JSON.parse(items[key]));
   });
 };
+/**
+ * Calculate the total calories of clicked items
+ */
 function calculate() {
   const items = document.getElementsByClassName('clicked');
   const itemList = [];
-  items.map((item) => itemList.push(parseInt(item.children.item(1).innerHTML)));
+  items.map((item) => itemList.push(parseInt(item.children.item(1).innerHTML, 10)));
   const calories = calculateCalories(itemList);
   document.getElementById('displayCals').innerHTML = calories;
 }
+/**
+ * On Item clicked, update the item's class to reflect if the item is selcted or not
+ * @param elem the HTMLElement object that called clickHandler
+ */
 function clickHandler(elem) {
   if (elem.classList.contains('clicked')) {
     elem.classList.remove('clicked');
@@ -39,3 +57,20 @@ function clickHandler(elem) {
     elem.classList.add('clicked');
   }
 }
+
+/* Style used: airbnb. FLAKE8 can not be used for JavaScript. ESLint output:
+
+C:\Users\Thomas\Documents\3010Project\SYSC-3010\public\buttonFunctions.js
+   4:10  error  'scan' is defined but never used          no-unused-vars
+  12:10  error  'rescan' is defined but never used        no-unused-vars
+  25:10  error  'getItems' is defined but never used      no-unused-vars
+  36:5   error  'addItem' is not defined                  no-undef
+  42:10  error  'calculate' is defined but never used     no-unused-vars
+  46:20  error  'calculateCalories' is not defined        no-undef
+  53:10  error  'clickHandler' is defined but never used  no-unused-vars
+
+✖ 7 problems (7 errors, 0 warnings)
+
+The no-unused-vars error is being ignored because the functions are used in an HTML file.
+The no-undef error is being ignored as addItem and calculateCalories is from another javascript file.
+*/
