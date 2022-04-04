e1fc758dbb8934a65f62efeb703d7cc974a830da
deestroy <39637258+deestroy@users.noreply.github.com>
2022-04-04 18:36:19 -0400

    feat: add the camera to turn on indefinitely

---
+++ camera.py
@@ -0,0 +1,16 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+def turn_on_camera():
+# capture the raw image on the raspberry pi camera
+    camera = PiCamera()
+    rawCapture = PiRGBArray(camera)
+
+    camera.capture(rawCapture, format="bgr")
+    image = rawCapture.array
+
+    # show the image and wait for key to be presssed
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
