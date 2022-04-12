97b2162153549bcbe88202a3d35513e87ac051b2
Thomas <thomasknechtel@outlook.com>
2022-04-11 21:26:29 -0400

    removed contents outside folder

--- public/calculateCalories.js
+++
@@ -1,9 +0,0 @@
-function calculateCalories(calorieList){
-    var total =0
-    for(calories of  calorieList){
-        if(typeof calories == 'number'){
-              total +=calories
-        }
-    }
-    return total
-}
--- readme.md
+++
@@ -1,2 +0,0 @@
-# End-to-End Communication of Project
-  Contains the code to implement the messages in https://github.com/deestroy/SYSC-3010/blob/comunication/DeploymentDiagram.PNG
